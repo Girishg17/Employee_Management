a956265fbca99d6deaf513aaa286319bc8e9fc5b
Girish G Gonda <girishggonda@Girishs-MacBook-Pro.local>
2024-10-14 17:10:52 +0530

    created entity employee

---
+++ Employee_Management/src/Entity/Employee.java
@@ -0,0 +1,18 @@
+package Entity;
+
+import java.util.Date;
+
+public class Employee {
+    public String firstName;
+    public  String lastName;
+    public Date dateOfBirth;
+    public double experience;
+
+    public  Employee(String firstName,String lastName,Date dateOfBirth,double experience){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.dateOfBirth=dateOfBirth;
+        this.experience=experience;
+    }
+
+}
