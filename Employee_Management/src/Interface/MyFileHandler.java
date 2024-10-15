870a7b3a5d356147e7dd3da8231024bc1396f60c
Girish G Gonda <girishggonda@Girishs-MacBook-Pro.local>
2024-10-15 10:29:26 +0530

    interface

--- Employee_Management/src/Interface/MyFileHandler.java
+++ Employee_Management/src/Interface/MyFileHandler.java
@@ -1,9 +1,8 @@
 package Interface;
 
-import Repository.MyCollection;
 
 public interface MyFileHandler {
 
-    public void write(MyCollection collection);
-    public void read(MyCollection collection);
+    public void write();
+    public void read();
 }
